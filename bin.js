210f03e7a49e323f918d0b508a2a884f63f4217d
Jannis R <mail@jannisr.de>
2018-05-23 12:34:35 +0200

    implementation :sparkles:

--- bin.js
+++ bin.js
@@ -1,4 +1,57 @@
 #!/usr/bin/env node
 'use strict'
 
-// todo
+const mri = require('mri')
+
+const pkg = require('./package.json')
+
+const argv = mri(process.argv.slice(2), {
+	boolean: [
+		'help', 'h',
+		'version', 'v',
+		'json', 'j'
+	]
+})
+
+if (argv.help || argv.h) {
+	process.stdout.write(`
+Usage:
+    location
+    location --json
+
+Options:
+    --json  -j  Output data as JSON. Default: false
+\n`)
+	process.exit()
+}
+
+if (argv.version || argv.v) {
+	process.stdout.write(pkg.name + ' ' + pkg.version + '\n')
+	process.exit(0)
+}
+
+const getLocation = require('@derhuerst/location')
+const {inspect} = require('util')
+
+getLocation((err, loc) => {
+	if (err) {
+		console.error(err)
+		process.exitCode = 1
+		return
+	}
+
+	if (argv.json || argv.j) {
+		process.stdout.write(JSON.stringify(loc) + '\n')
+		return
+	}
+
+	let {latitude: lat, longitude: long, precision: prec} = loc
+	lat = 'number' === typeof lat ? lat.toFixed(7) : util.inspect(lat)
+	long = 'number' === typeof long ? long.toFixed(7) : util.inspect(long)
+	prec = 'number' === typeof prec ? prec : util.inspect(prec)
+	process.stdout.write([
+		'latitude:  ' + lat,
+		'longitude: ' + long,
+		'precision: ' + prec
+	].join('\n') + '\n')
+})
